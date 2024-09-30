5d49a0408f94e25c376006f7a36dcb31a0e37aaf
shineinfosoft64 <shineinfosoft64@gmail.com>
2024-09-30 14:21:45 +0530

    Initital commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import PagesTitle from './Components/TitleOfPages/PagesTitle';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Add Funnels</h1>
+      <PagesTitle />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/TitleOfPages/PagesTitle.js
@@ -0,0 +1,50 @@
+import React from 'react'
+import * as React from 'react';
+import Tabs from '@mui/material/Tabs';
+import Tab from '@mui/material/Tab';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+
+// interface TabPanelProps {
+//   children?: React.Rea mctNode;
+//   index: number;
+//   value: number;
+// }
+
+function TabPanel(props) {
+  const { children, value, index, ...other } = props;
+
+  return (
+    <div
+      role="tabpanel"
+      hidden={value !== index}
+      id={`vertical-tabpanel-${index}`}
+      aria-labelledby={`vertical-tab-${index}`}
+      {...other}
+    >
+      {value === index && (
+        <Box sx={{ p: 3 }}>
+          <Typography>{children}</Typography>
+        </Box>
+      )}
+    </div>
+  );
+}
+
+function a11yProps(index) {
+  return {
+    id: `vertical-tab-${index}`,
+    'aria-controls': `vertical-tabpanel-${index}`,
+  };
+}
+
+
+const PagesTitle = () => {
+  return (
+    <div>
+      <h1></h1>
+    </div>
+  )
+}
+
+export default PagesTitle
